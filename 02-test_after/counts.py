de93a60c13ea1ec51bf6efebf7424f3fc73a4a81
akenihan <46343185+akenihan@users.noreply.github.com>
2019-01-03 12:36:37 +0000

    Lesson code

--- 02-test_after/counts.py
+++ 02-test_after/counts.py
@@ -4,7 +4,7 @@ def count_upper_case(message):
         if c.isupper():
             count += 1
     return count
-
+    
 assert count_upper_case("") == 0, "Empty string"
 assert count_upper_case("A") == 1, "One upper case"
 assert count_upper_case("a") == 0, "One lower case"
