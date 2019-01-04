7595937efef6d8aa67afe80693ff09987cf29c0d
akenihan <46343185+akenihan@users.noreply.github.com>
2019-01-04 09:56:23 +0000

    Create pyotest.py

---
+++ 06-build_your_own_test_framework/challenge_one/pyotest.py
@@ -0,0 +1,20 @@
+def number_of_evens(numbers):
+    return 0
+
+def test_are_equal(actual, expected):
+    assert expected == actual, "Expected {0}, got {1}".format(expected, actual)
+    
+def test_not_equal(a, b):
+    assert a != b, "Did not expect {0} but got {1}".format(a, b)
+    
+def test_is_in(collection, item):
+    assert item in collection, "{0} name does not contain {1}".format(collection, item)
+
+# Test to fail the `test_are_equal` function
+# test_are_equal(number_of_evens([1,2,3,4,5]), 2)
+
+# Test to fail the `test_not_equal` function
+# test_not_equal(0, 0)
+
+# Test to fail the `test_is_in` function
+# test_is_in([1], 2)
